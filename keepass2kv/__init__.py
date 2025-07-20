723dcb3641f4ec01e6ed27c7111ace704c66a5b1
vit0r <vitornascimentoaraujo@gmail.com>
2025-07-20 00:56:53 -0300

    update 1

--- keepass2kv/__init__.py
+++ keepass2kv/__init__.py
@@ -2,5 +2,5 @@ from .keepass2kv import Keepass2Kv
 
 if __name__ == "main":
     kv = Keepass2Kv()
-    kv_secrets = kv.export()
-    kv.import_kv(kv_secrets)
+    kv.kv_secrets = kv.export()
+    kv.import_kv()
